5495a54cf67e05ea545dd7663837b1b886cbe58b
Aquamankar <imakankshamnkr@gmail.com>
2025-04-05 11:32:22 +0530

    first commit

---
+++ src/main/java/com/gitCommands/GitBranchesApplication.java
@@ -0,0 +1,13 @@
+package com.gitCommands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBranchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBranchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitCommands/GitBranchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitCommands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBranchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
